4200613e9ac775ab8a8f5bd1e3a0ab725d58c71a
kushfj <kush@kush.com.fj>
2017-03-26 13:27:40 +1000

    added macros for free and malloc, modified comments for functions to follow standard, added prototypes for print_mac and reverse_bytes functions

--- include/utils.h
+++ include/utils.h
@@ -32,14 +32,64 @@
 #define _UTILS_H_
 
 
-/** Function to output the specified buffer as hexadecimal values to stdout
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/**
+ * Macro to invoke free on the pointer, it is assumed that the pointer was
+ * returned (RET) from the MALLOC macro
+ *
+ * @param PTR the pointer to memory to free
+ */
+#define FREE(PTR) \
+do \
+{ \
+  if (PTR) \
+  { \
+    free(PTR); \
+  } \
+} \
+while (0)
+
+
+/**
+ * Macro to invoke malloc and check that memory is allocated. If memory
+ * allocation fails, then exit the program.
+ *
+ * @param ret  the address of memory allocated if malloc is successful, else
+ *             unchanged
+ * @param type  the type of memory to cast, e.g., int, float, uint8_t, etc.
+ * @param size  the size_t of memory to allocate
+ */
+#define MALLOC(RET, TYPE, MEM_SIZE) \
+do \
+{ \
+  if (MEM_SIZE <= 0) \
+  { \
+    RET = NULL; \
+  } \
+  else if (NULL == (RET = (TYPE *)malloc(MEM_SIZE))) \
+  { \
+    fprintf(stderr, "ERROR: unable to allocate memory\n"); \
+    fflush(stderr); \
+    exit(EXIT_FAILURE); \
+  } \
+} \
+while (0)
+
+
+
+/** 
+ * Function to output the specified buffer as hexadecimal values to stdout
  *
  * @param data	- void * pointer to the data to dump
  * @param len	- unsigned int representing the length of the data
  */
 void hex_dump(const void *data, const unsigned int len);
 
-/** Function to return the actual number of bytes used by an unsigned 32-bit 
+/** 
+ * Function to return the actual number of bytes used by an unsigned 32-bit 
  * integer
  *
  * @param num	- uint32_t integer
@@ -48,7 +98,15 @@ void hex_dump(const void *data, const unsigned int len);
  */
 uint8_t num_bytes_for_ui32(const uint32_t num);
 
-/** Function to return a uint32_t value representing the reversed order of 
+/**
+ * Function to print value as hexadecimal hardware MAC address
+ *
+ * @param mac - pointer to hardware mac address
+ */
+void print_mac(const uint8_t *mac);
+
+/** 
+ * Function to return a uint32_t value representing the reversed order of 
  * bytes for the specified uint32_t number.
  *
  * @param num	- uint32_t integer
@@ -57,7 +115,23 @@ uint8_t num_bytes_for_ui32(const uint32_t num);
  */
 uint32_t reverse_ui32(uint32_t num);
 
-/** Function to convert the time value specified as a long into into a 4-byte 
+
+/**
+ * Function to reverse the bytes stored at a source location into the
+ * destination location. The source and destination locations must not overlap
+ * The destination location is over-written with bytes from the source location
+ * in reverse order, or left unchanged if num_bytes is 0.
+ *
+ * @param src pointer to the source location
+ * @param dst pointer to the destination location, must be initialised and
+ *             capable of storing num_bytes and not overlapping the source 
+ *             location
+ * @param num_bytes number of bytes to reverse
+ */
+void reverse_bytes(uint8_t *src, uint8_t *dst, size_t num_bytes);
+
+/** 
+ * Function to convert the time value specified as a long into into a 4-byte 
  * value. The 4-byte value is put into the buffer specified.
  *
  * @param tv	- long int representing the time value
@@ -65,7 +139,8 @@ uint32_t reverse_ui32(uint32_t num);
  */
 void time_to_bytes(const long int tv, uint8_t *addr);
 
-/** Function to convert the time value and quality struct specified as a 
+/** 
+ * Function to convert the time value and quality struct specified as a 
  * timevalq_t into an 8-byte value. The 8-byte value is put into the buffer 
  * specified.
  *
@@ -74,7 +149,8 @@ void time_to_bytes(const long int tv, uint8_t *addr);
  */
 void timevalq_to_bytes(const timevalq_t *t, uint8_t *addr);
 
-/** Function to convert the number specified as a uint32_t into bytes. The 
+/** 
+ * Function to convert the number specified as a uint32_t into bytes. The 
  * bytes are put into the buffer specified.
  *
  * @param num	- uint32_t representing the number
